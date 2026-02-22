376ce7adec3395a93ca877f33cb3ec7926e0750d
Asterthorck <amejias98@hotmail.es>
2026-02-21 19:43:04 -0600

    Creacion de Archivos
    Se creo el archivo .HTML y el folder JS con el archivo de .JS

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btnGit = document.getElementById("btnGit");
+    const btnInfo = document.getElementById("btnInfo");
+
+    btnGit.addEventListener("click", () => {
+        window.open("https://github.com/", "_blank");
+    });
+
+    btnInfo.addEventListener("click", () => {
+        alert(
+            "Git es un sistema de control de versiones que te permite " +
+            "guardar cambios, trabajar en equipo y manejar versiones de código."
+        );
+    });
+
+});
